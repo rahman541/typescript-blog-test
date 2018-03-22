80035638f409481596af20ede66084f1056bae46
Rahman <rahmanromli@yahoo.com>
2018-03-22 23:48:12 +0800

    Add router file

---
+++ router/v1.ts
@@ -0,0 +1,13 @@
+import * as express from 'express'
+import { getAllPosts, getPostById, createPost, updatePost, deletePost } from '../controllers/PostController'
+
+export default (app) => {
+  const apiRoutes = express.Router()
+  const postRoutes = express.Router()
+
+  /// POST ROUTES ///
+  apiRoutes.use('/posts', postRoutes)
+  postRoutes.get('/', getAllPosts)
+
+  apiRoutes.use('api', apiRoutes)
+}
